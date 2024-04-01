57645d05bf087e2ae011a00870aa835c49b28169
Hessen Nasser <92742551+hessennasser@users.noreply.github.com>
2024-04-01 16:45:42 +0200

    edit table of product in purchases

--- src/app/[lng]/purchases/page.jsx
+++ src/app/[lng]/purchases/page.jsx
@@ -84,13 +84,13 @@ const Page = () => {
                                                 <table className="min-w-full divide-y divide-gray-200">
                                                     <thead className="bg-gray-50">
                                                         <tr>
-                                                            <th scope="col" className="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            <th scope="col" className="px-6 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                                 {t('Product Name')}
                                                             </th>
-                                                            <th scope="col" className="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            <th scope="col" className="px-6 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                                 {t('Price')}
                                                             </th>
-                                                            <th scope="col" className="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            <th scope="col" className="px-6 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                                 {t('Quantity')}
                                                             </th>
                                                         </tr>
